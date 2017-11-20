025a618ead5426595b8e491cb236e34b25350587
github username <github email address>
2017-11-20 10:58:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+  get '/' do
+    # some code to get all the medicines
+    "Hello, World!"
+    # some code to render the correct HTML page
+ end
+
 
 end
